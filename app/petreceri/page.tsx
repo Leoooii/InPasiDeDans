20330b976a4a13fb3fa29c3aafd4cc2e7e9dbf17
Leoooii <lioneh39@gmail.com>
2025-05-12 12:07:02 +0300

    fb removed

--- app/petreceri/page.tsx
+++ app/petreceri/page.tsx
@@ -199,18 +199,6 @@ export default function Petreceri() {
               </Card>
             ))}
           </div>
-          <div className="mt-12 w-full flex justify-center">
-            <iframe
-              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fscoaladedansinpasidedans&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-              width="500"
-              height="500"
-              style={{ border: 'none', overflow: 'hidden' }}
-              scrolling="no"
-              frameBorder="0"
-              allowFullScreen={true}
-              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-            ></iframe>
-          </div>
         </div>
       </div>
     </div>
